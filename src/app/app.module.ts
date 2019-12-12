b7e608ec15790e29971627aeb53b64d88c60936f
Alexander Miller <alecwmiller@live.co.uk>
2019-12-12 14:35:20 +0000

    Added project structure and basic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyComponent } from './currency/currency.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule,

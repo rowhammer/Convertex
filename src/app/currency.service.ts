444642de8d1e1657e5f74fc71564d566a1ffe131
Alec Miller <alecwmiller@live.co.uk>
2019-12-20 17:19:47 +0000

    Updated security for API

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -15,7 +15,7 @@ export class CurrencyService {
   }
 
   getExchangeRate(from: string, to: string) {
-    return this.http.get(`http://free.currencyconverterapi.com/api/v5/convert?q=${from}_${to}&compact=y&apiKey=${this.apikey}`).toPromise();
+    return this.http.get(`https://free.currencyconverterapi.com/api/v5/convert?q=${from}_${to}&compact=y&apiKey=${this.apikey}`).toPromise();
   }
 
 }
